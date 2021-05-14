8da85cb275a6225715f63d1ea8a09b7921568e5b
Pankaj kumar <77680413+pankaj1507@users.noreply.github.com>
2021-05-15 02:52:16 +0530

    Update List_Comprehensions.py

--- Python/Basic_Data_Types/List_Comprehensions.py
+++ Python/Basic_Data_Types/List_Comprehensions.py
@@ -26,5 +26,8 @@ Y = int(raw_input())
 Z = int(raw_input())
 N = int(raw_input())
 
+x += 1
+y += 1
+z += 1
 ans = [[i, j, k] for i in range(X + 1) for j in range(Y + 1) for k in range(Z + 1) if i + j + k != N]
 print ans
